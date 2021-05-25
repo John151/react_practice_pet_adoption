146215d16328147e4065a67e1e8706039ffe153d
John Cokins <il7148pg@go.minnstate.edu>
2021-05-24 23:49:08 -0500

    created homepage html

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
-
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt me!"),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bird",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Sudo",
-//       animal: "Dog",
-//       breed: "Rat Terrier",
-//     }),
-//   ]);
-// };
 
 const App = () => {
   return (
@@ -34,4 +12,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
