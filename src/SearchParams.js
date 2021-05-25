0855ad427b52de9df19346b36be2d443d7cd864f
John Cokins <il7148pg@go.minnstate.edu>
2021-05-25 01:35:32 -0500

    added hook to bind location input

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,1 +1,23 @@
-const SearchParams = () => {};
+import { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="locataion">
+          Location
+          <input
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
